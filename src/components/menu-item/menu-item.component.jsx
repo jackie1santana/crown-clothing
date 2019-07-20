76676f35fe2daa425acfeea411ef8598c6d927dc
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2019-07-20 13:52:03 -0500

    large application

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import './menu-item.styles.scss'
+import React from 'react';
 
-const MenuItem = ({ title, imageUrl }) =>  (
-    <div style={{backgroundImage: `url(${imageUrl})`}} className='menu-item'>
-        <div className='content'>
-          <div className='title'>{title}</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-)
+import './menu-item.styles.scss';
 
-
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
 
 export default MenuItem;
